e036626e71b3f9633277b7382196e37f8118ac6b
fraternity25 <huseyinsacikay0@gmail.com>
2025-08-28 16:59:02 +0300

    modified:   components/Toast/index.js modified:   lib/dataService/notifications.js modified:   lib/dataService/users.js modified:   pages/notifications/index.js

--- components/Toast/index.js
+++ components/Toast/index.js
@@ -1,18 +1,46 @@
 import { useEffect, useState, useRef } from 'react';
 
-export function ToastMessages({messages}) {
+const colors = {
+  error: {
+    bg: "bg-red-100 border-red-400 text-red-800",
+    sliderBg: "bg-red-500",
+  },
+  success: {
+    bg: "bg-green-100 border-green-400 text-green-800",
+    sliderBg: "bg-green-500",
+  },
+  info: {
+    bg: "bg-blue-100 border-blue-400 text-blue-800",
+    sliderBg: "bg-blue-500",
+  },
+  warning: {
+    bg: "bg-yellow-100 border-yellow-400 text-yellow-800",
+    sliderBg: "bg-yellow-500",
+  }
+};
+colors.default = colors.info;
+
+export function ToastMessages({messages, type="info"}) {
+  const { bg } = colors[type] || colors.default;
+
   return (
-    messages.map((msg, i) => {
-      const Tag = msg.type;
-      if (Tag == "ul") {
-        return (
-          <Tag key={i} className="list-disc ml-4">
-            {msg.content.map((li, j) => <li key={j}>{li}</li>)}
-          </Tag>
-        );
+    <div 
+      className={`border ${bg} px-4 
+        py-2`
       }
-      return <Tag key={i}>{msg.content}</Tag>;
-    })
+    >
+      {messages.map((msg, i) => {
+          const Tag = msg.type;
+          if (Tag == "ul") {
+            return (
+              <Tag key={i} className="list-disc ml-4">
+                {msg.content.map((li, j) => <li key={j}>{li}</li>)}
+              </Tag>
+            );
+          }
+          return <Tag key={i}>{msg.content}</Tag>;
+      })}
+    </div>
   );
 }
 
@@ -25,26 +53,7 @@ export default function Toast({ messages = [], time = 4000, type="info", onClose
   const remainingTimeRef = useRef(time);
   const onCloseRef = useRef(onClose);
 
-  const colors = {
-    error: {
-      bg: "bg-red-100 border-red-400 text-red-800",
-      sliderBg: "bg-red-500",
-    },
-    success: {
-      bg: "bg-green-100 border-green-400 text-green-800",
-      sliderBg: "bg-green-500",
-    },
-    info: {
-      bg: "bg-blue-100 border-blue-400 text-blue-800",
-      sliderBg: "bg-blue-500",
-    },
-    default: { // Using a "default" key for the last case
-      bg: "bg-yellow-100 border-yellow-400 text-yellow-800",
-      sliderBg: "bg-yellow-500",
-    },
-  };
-  colors.info = colors.default;
-  const { bg, sliderBg } = colors[type] || colors.default;
+  const { sliderBg } = colors[type] || colors.default;
 
   // onClose ref'ini güncelle
   useEffect(() => {
@@ -94,8 +103,7 @@ export default function Toast({ messages = [], time = 4000, type="info", onClose
 
   return (
     <div 
-      className={`relative border ${bg} px-4 
-        py-2 rounded shadow-lg z-50 w-full 
+      className={`relative rounded shadow-lg z-50 w-full 
         max-w-md overflow-hidden transition-opacity 
         duration-500 ${ isClosing ? 
           'opacity-0' : 'opacity-100'
@@ -106,18 +114,16 @@ export default function Toast({ messages = [], time = 4000, type="info", onClose
       style={{ '--toast-duration': `${time}ms` }}
     >
       {/* Messages */}
-      <div className="py-2">
-        <ToastMessages messages={messages} />
+      <ToastMessages messages={messages} type={type} />
 
-        {/*
-          TO DO: Add a <Link> here that says "see this notification" 
-                 that will navigate to notifications/${id}. This link
-                 will be in line with the following element
-          TO DO: Add a div or any other element that will show the 
-                 remainingTime state at the bottom left but will be above
-                 the slider bar
-        */}
-      </div>
+      {/*
+        TO DO: Add a <Link> here that says "see this notification" 
+                that will navigate to notifications/${id}. This link
+                will be in line with the following element
+        TO DO: Add a div or any other element that will show the 
+                remainingTime state at the bottom left but will be above
+                the slider bar
+      */}
 
       {/* Sliding bar with pause control */}
       <div className={`absolute bottom-0 left-0 h-1 ${sliderBg} animate-slide-right-to-left ${
--- pages/notifications/index.js
+++ pages/notifications/index.js
@@ -4,12 +4,98 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
+function HistoryHeader({self, user}) {
+  return (
+    <h2 className="text-sm font-semibold text-gray-700">
+      <div className='flex justify-between items-center'>
+        <div 
+          className={`flex items-center gap-2 
+            ${self ? "text-md font-bold text-gray-800" : ""}
+          `}
+        >
+          {self ? "Your notification history" : `${user.name} (${user.email})`}
+          {/*unreadCount > 0 && `(${unreadCount} unread)`*/}
+        </div>
+        <div>{user.role}</div>
+      </div>
+    </h2>
+  );
+}
+
+function History({user , onDelete}) {
+  const isAdmin = user.role === "admin";
+  const isFindNotif = user.notifications?.length > 0;
+
+  const content = !isFindNotif ? (
+      <div className="text-center py-6 bg-white">
+        <p className="text-gray-500">No notifications found</p>
+      </div>
+    ) : (
+      <div className="bg-gray-50 rounded-md py-2">
+        <ul className="divide-y divide-gray-200">
+          {user.notifications.map((notif) => (
+            <li
+              key={notif.id}
+              className={`p-4 hover:bg-gray-100 ${
+                !notif.is_read ? "bg-blue-50" : ""
+              }`}
+            >
+              <div className="flex items-center justify-between">
+                <div>
+                  <ToastMessages 
+                    messages={stringToToastMessages(notif.message)} 
+                    type={notif.type} 
+                  /> 
+                  <p className="text-gray-500 text-xs mt-1">
+                    {new Date(notif.created_at).toLocaleString("en-US", {
+                      hour12: false,
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+                <div className="flex items-center space-x-2">
+                  {/*!notif.is_read && (
+                    <button
+                      onClick={() => markAsRead(notif.id)}
+                      className="text-sm text-blue-600 hover:text-blue-800"
+                    >
+                      Mark as read
+                    </button>
+                  )*/}
+
+                  {isAdmin && 
+                    <button
+                      className="inline-flex items-center p-2 rounded-md cursor-pointer border border-transparent 
+                                shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 
+                                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                      onClick={() => onDelete(notif.id)}
+                    >
+                      Delete
+                    </button>
+                  }
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  return content;
+}
+
 export default function NotificationsContent() {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const [allNotifications, setAllNotifications] = useState([]);
+  const [allNotifications, setAllNotifications] = useState([]); // [{ id, name, email, role, notifications: [...] }, ...]
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const isAdmin = session?.user?.role === "admin";
+  //console.log("allNotifications", allNotifications);
 
   useEffect(() => {
     if (status === "unauthenticated" && !session) {
@@ -18,19 +104,26 @@ export default function NotificationsContent() {
   }, [status, session, router]);
 
   useEffect(() => {
-    if (session?.user?.id) {
-      fetchNotifications();
+    if(session) {
+      if (session.user.role === "admin") {
+        fetchNotifications();
+      }
+      else {
+        fetchNotifications(session.user.id);
+      }
     }
   }, [session]);
 
-  const fetchNotifications = async () => {
+  const fetchNotifications = async (userId=null) => {
     try {
       setLoading(true);
-      const response = await fetch(`/api/notifications`);
+      const response = await fetch(
+        userId ? `/api/notifications/${userId}` : `/api/notifications`
+      );
       if (!response.ok) throw new Error("Failed to fetch notifications");
 
       const data = await response.json();
-      setAllNotifications(data); // artık [{ user_id, notifications: [...] }, ...]
+      setAllNotifications(userId ? [data] : data);
     } catch (err) {
       setError(err.message);
     } finally {
@@ -38,7 +131,7 @@ export default function NotificationsContent() {
     }
   };
 
-  const markAsRead = async (notificationId) => {
+  /* const markAsRead = async (notificationId) => {
     try {
       const response = await fetch(`/api/notifications/${notificationId}`, {
         method: "PUT",
@@ -56,7 +149,7 @@ export default function NotificationsContent() {
     } catch (err) {
       console.error(err);
     }
-  };
+  }; */
 
   const deleteNotification = async (notificationId) => {
     try {
@@ -95,80 +188,36 @@ export default function NotificationsContent() {
   }
 
   // tüm kullanıcılar için unreadCount
-  const unreadCount = allNotifications.reduce(
+  /* const unreadCount = allNotifications.reduce(
     (sum, user) => sum + user.notifications.filter((n) => !n.is_read).length,
     0
-  );
+  ); */
+
+  const self = allNotifications.find((u) => u.id == session?.user?.id);
+  console.log("self = ", self);
 
   return (
     <div className="max-w-7xl py-4 mx-auto px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center sm:justify-between">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-bold text-black">Notifications</h1>
-          <p className="mt-2 text-md font-semibold text-gray-800">
-            Your notification history{" "}
-            {unreadCount > 0 && `(${unreadCount} unread)`}
-          </p>
+          <h1 className="text-xl font-bold text-black mb-4">Notifications</h1>
+          <HistoryHeader self={true} user={self || session?.user} />
+          <History user={self || session?.user} onDelete={deleteNotification} />
         </div>
       </div>
 
-      <div className="mt-8 flex flex-col space-y-4">
-        {allNotifications.map((user) =>
-          user.notifications.length === 0 ? (
-            <div className="text-center py-12 bg-white">
-              <p className="text-gray-500">No notifications found</p>
+      {isAdmin && 
+        <div className="mt-4 flex flex-col space-y-2">
+          {allNotifications
+            .filter((user) => user.id != session?.user?.id)
+            .map((user) =>
+            <div key={user.id} >
+              <HistoryHeader self={false} user={user} />
+              <History user={user} onDelete={deleteNotification}/>
             </div>
-          ) : (
-            <div key={user.user_id} className="bg-gray-50 rounded-md p-4">
-              <h2 className="text-sm font-semibold text-gray-700 mb-2">
-                User ID: {user.user_id}
-              </h2>
-              <ul className="divide-y divide-gray-200">
-                {user.notifications.map((notif) => (
-                  <li
-                    key={notif.id}
-                    className={`p-4 hover:bg-gray-100 ${
-                      !notif.is_read ? "bg-blue-50" : ""
-                    }`}
-                  >
-                    <div className="flex items-center justify-between">
-                      <div>
-                        <ToastMessages messages={stringToToastMessages(notif.message)} /> 
-                        <p className="text-gray-500 text-xs mt-1">
-                          {new Date(notif.created_at).toLocaleString("en-US", {
-                            hour12: false,
-                            year: "numeric",
-                            month: "2-digit",
-                            day: "2-digit",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </p>
-                      </div>
-                      <div className="flex items-center space-x-2">
-                        {!notif.is_read && (
-                          <button
-                            onClick={() => markAsRead(notif.id)}
-                            className="text-sm text-blue-600 hover:text-blue-800"
-                          >
-                            Mark as read
-                          </button>
-                        )}
-                        <button
-                          onClick={() => deleteNotification(notif.id)}
-                          className="text-sm text-red-600 hover:text-red-800"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )
-        )}
-      </div>
+          )}
+        </div>
+      }
     </div>
   );
 }
